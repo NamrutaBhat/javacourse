84f25200b6fc95109a10f259accea98b0f7554bc
NamrutaBhat <79894256+NamrutaBhat@users.noreply.github.com>
2021-03-11 10:40:36 +0530

    Add files via upload

---
+++ Method.java
@@ -0,0 +1,18 @@
+class Method{
+   
+  public static void main(String[] args){
+ 
+     add(10,20);
+    
+
+}
+ 
+static void  add(int num1,int num2){
+
+ int result=num1+num2;
+ System.out.println(result);
+
+ }
+
+}
+
