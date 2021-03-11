65ddd62893fbf057742cc720875dd1efcc47dfa4
unknown <namrutavb@gmail.com>
2021-03-11 10:54:29 +0530

    Adds loops.java file

---
+++ ForeachLoops.java
@@ -0,0 +1,13 @@
+class ForeachLoops{
+
+    public static void main(String[] args){
+
+        String[] trainees={"namruta","tejas","Shweta","AKshata"};
+
+           for(String trainee:trainees){
+                
+                System.out.println("Hi I am "+trainee);
+              }
+    }
+}
+    
