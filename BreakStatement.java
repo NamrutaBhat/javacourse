5632605a080fd59d85d228cdd6128c9c78eac6ff
NamrutaBhat <79894256+NamrutaBhat@users.noreply.github.com>
2021-03-17 10:02:31 +0530

    Add files via upload

---
+++ BreakStatement.java
@@ -0,0 +1,15 @@
+class BreakStatement{
+ 
+ public static void main(String[] args){
+
+     for(int i=0;i<=10;i++)  {
+
+          if(i==6){
+            System.out.println("end of loop");
+break;
+           }
+  
+           System.out.println(i);
+      }
+  }
+}
