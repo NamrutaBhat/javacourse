c013d0aa8813c2e62d1be6c2f64b61a70610878a
NamrutaBhat <79894256+NamrutaBhat@users.noreply.github.com>
2021-03-18 09:37:55 +0530

    Add files via upload

---
+++ BreakStatementDoWhile.java
@@ -0,0 +1,24 @@
+class BreakStatementDoWhile{
+ 
+ public static void main(String[] args){
+     int n=20;
+     do
+ {
+ 	if(n==22){
+            System.out.println("system is terminated");
+            break;
+           }           
+
+            System.out.println(n);
+             n++;
+            }while(n<=30); 
+         
+	
+         
+
+         
+  
+           
+      
+  }
+}
